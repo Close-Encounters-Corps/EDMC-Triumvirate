8637c8e95f68558375f9160a865a4ec384cbc62f
Slava <slavakazakov10@gmail.com>
2019-04-09 11:02:33 +0300

    смена номера версии

--- load.py
+++ load.py
@@ -64,7 +64,7 @@ this.nearloc = {
 myPlugin = "EDMC-Triumvirate"
 
 
-this.version="1.0.1"
+this.version="1.0.2"
 this.client_version="{}.{}".format(myPlugin,this.version)
 this.body_name=None
     
