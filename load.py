5e555ae539d2ade725e1dd7bd3f19f2cbd1a4a52
слава казаков <slava.kazakov10@gmail.com>
2019-06-12 02:23:06 +0300

    создан шаблон системы FF из новостей, и импортирован в плагин

--- load.py
+++ load.py
@@ -16,7 +16,7 @@ from modules import legacyCanonn as Clegacy
 from modules import clientreport
 from modules import fssreports
 from modules import patrol
-#from modules import friendfoe as FF
+from modules import friendfoe as FF
 from modules.systems import Systems
 from modules.debug import Debug
 from modules.debug import debug
