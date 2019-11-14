ecba428cfa8514d55b3a47c9138caf8abc9413cf
VAKazakov <slavakazakov10@gmail.com>
2019-11-14 22:46:47 +0300

    Поставил версию 1.3.0

--- load.py
+++ load.py
@@ -58,7 +58,7 @@ this.nearloc = {
 myPlugin = 'EDMC-Triumvirate'
 
 
-this.version='1.1.0'
+this.version='1.3.0'
 this.SQNag=0
 this.client_version='{}.{}'.format(myPlugin,this.version)
 this.body=None
