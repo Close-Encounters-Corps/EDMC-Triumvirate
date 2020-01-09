a4139feb1523a15e786d3c43e6d0b79afd22c7df
VAKazakov <47143965+VAKazakov@users.noreply.github.com>
2020-01-09 17:04:08 +0300

    1.1.12

--- load.py
+++ load.py
@@ -57,7 +57,7 @@ this.nearloc = {
 myPlugin = 'EDMC-Triumvirate'
 
 
-this.version='1.1.11'
+this.version='1.1.12'
 this.SQNag=0
 this.client_version='{}.{}'.format(myPlugin,this.version)
 this.body=None
