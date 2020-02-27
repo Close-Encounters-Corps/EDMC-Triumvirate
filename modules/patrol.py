10e49dfa021d44c8504795543dc9aefb99d01f7f
VAKazakov <slavakazakov10@gmail.com>
2020-02-27 21:27:15 +0300

    Добавлен код дебаг, который позволит поймать два запуска патруля

--- modules/patrol.py
+++ modules/patrol.py
@@ -662,10 +662,10 @@ class CanonnPatrol(Frame):
             self.SQID = SQ
         else: self.SQID = "None"
         debug("SQID In Patrol IS " + self.SQID)
-
+    import traceback
     def download(self):
         while self.SQID is None:
-            debug("Waiting CMDR's squadron")
+            debug("Waiting CMDR's squadron {}:\n{}".format(self,traceback.print_stack(Limit=None)))
             time.sleep(5)
         debug("Download Patrol Data")
         
