f83b08ef02b63eadfbd82bb140f1dbd5751c804a
VAKazakov <slavakazakov10@gmail.com>
2020-02-27 21:18:47 +0300

    Изменен код получения частей патруля на отказоустойчивый

--- modules/patrol.py
+++ modules/patrol.py
@@ -458,8 +458,12 @@ class CanonnPatrol(Frame):
         url = "https://docs.google.com/spreadsheets/d/e/2PACX-1vQQZFJ4O0nb3L1WJk5oMEPJrr1w5quBSnPRwSbz66XCYx0Lq6aAexm9s1t8N8iRxpdbUOtrhKqQMayY/pub?gid=0&single=true&output=tsv"        
         with closing(requests.get(url, stream=True)) as r:
             r.encoding = 'utf-8'
-            reader = csv.reader(r.iter_lines(decode_unicode=True), delimiter='\t')#
-            next(reader)
+            try:
+                reader = csv.reader(r.content.splitlines(), delimiter='\t') # .decode('utf-8')
+                next(reader)
+            except :
+                reader = csv.reader(r.content.decode('utf-8').splitlines(), delimiter='\t') # 
+                next(reader)
             for row in reader:
                 debug(row)
                 squadron,system,x,y,z,TINF,TFAC,Description = row
@@ -579,8 +583,12 @@ class CanonnPatrol(Frame):
         canonnpatrol = []
                
         with closing(requests.get(url, stream=True)) as r:
-            reader = csv.reader(r.iter_lines(decode_unicode=True), delimiter='\t')
-            next(reader)
+            try:
+                reader = csv.reader(r.content.splitlines(), delimiter='\t') # .decode('utf-8')
+                next(reader)
+            except :
+                reader = csv.reader(r.content.decode('utf-8').splitlines(), delimiter='\t') # 
+                next(reader)
             for row in reader:
                 
                 type = get(row,0)
@@ -605,8 +613,12 @@ class CanonnPatrol(Frame):
         canonnpatrol = []
         url = "https://docs.google.com/spreadsheets/d/e/2PACX-1vSMFJL2u0TbLMAQQ5zYixzgjjsNtGunZ9-PPZFheB4xzrjwR0JPPMcdMwqLm8ioVMp3MP4-k-JsIVzO/pub?gid=282559555&single=true&output=tsv"  
         with closing(requests.get(url, stream=True)) as r:
-            reader = csv.reader(r.iter_lines(decode_unicode=True), delimiter='\t')
-            next(reader)
+            try:
+                reader = csv.reader(r.content.splitlines(), delimiter='\t') # .decode('utf-8')
+                next(reader)
+            except :
+                reader = csv.reader(r.content.decode('utf-8').splitlines(), delimiter='\t') # 
+                next(reader)
             for row in reader:
 
                 id,	enabled, description, type, link = row
