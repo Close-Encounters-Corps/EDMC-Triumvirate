43e62645385bd9c867d07f7c3dda0a6e2231237c
Slava Kazakov <slavakazakov10@gmail.com>
2020-03-06 22:01:09 +0300

    обновлен код дебага для предотвращения перекрытия записей

--- modules/debug.py
+++ modules/debug.py
@@ -8,50 +8,51 @@ except: #py2
 import myNotebook as nb
 from config import config
 import datetime 
+import sys
 
 class Debug:
-        
-    debugvar=tk.IntVar(value=config.getint("CanonnDebug"))
-    debugswitch = debugvar.get()
-    client="Triumvirate"
-    
+    debugvar = tk.IntVar(value=config.getint("CanonnDebug"))
+    client = "Triumvirate"
+
     @classmethod
-    def setClient(cls,client):
-        Debug.client=client
-            
+    def setClient(cls, client):
+        Debug.client = client
+
     @classmethod
-    def p(cls,value):
-        print("{} [{}] {}".format(datetime.datetime.now(),Debug.client,str(value)))
-            
+    def p(cls, value):
+        print("{} [{}] {}".format(datetime.datetime.now(), Debug.client, str(value)))
+        sys.stdout.flush()
+
     @classmethod
-    def debug(cls,value):
-        if cls.debugswitch == 1:
+    def debug(cls, value):
+        if cls.debugvar.get() == 1:
             cls.p(value)
 
-        
     @classmethod
-    def plugin_prefs(cls, parent,client,gridrow):
+    def plugin_prefs(cls, parent, client, gridrow):
         "Called to get a tk Frame for the settings dialog."
 
-        cls.debugvar=tk.IntVar(value=config.getint("CanonnDebug"))
-        Debug.client=client
-        
+        cls.debugvar = tk.IntVar(value=config.getint("CanonnDebug"))
+        Debug.client = client
+
         frame = nb.Frame(parent)
         frame.columnconfigure(1, weight=1)
-        frame.grid(row = 0, column = 0,sticky="NSEW")
-        
-        nb.Checkbutton(frame, text="Включить отладку", variable=cls.debugswitch).grid(row = 0, column = 0,sticky="NW")
-        
+        frame.grid(row=0, column=0, sticky="NSEW")
+
+        nb.Checkbutton(frame, text="Включить отладку", variable=cls.debugvar).grid(row=0, column=0, sticky="NW")
+
         return frame
-        
-    @classmethod    
+
+    @classmethod
     def prefs_changed(cls):
         "Called when the user clicks OK on the settings dialog."
-        config.set('CanonnDebug', cls.debugswitch)      
-        
+        config.set('CanonnDebug', cls.debugvar.get())
+
+
 def debug(value):
     Debug.debug(value)
-    
+
+
 def error(value):
-    Debug.p(value)    
+    Debug.p(value) 
 
