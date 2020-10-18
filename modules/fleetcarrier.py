9a36c497b33a9c067ee62332ac25e20b0532faa8
Igor Ovsyannikov <kamish@outlook.com>
2020-10-18 13:59:58 +0300

    Исправил отправку данных о прыжке ФК

--- modules/fleetcarrier.py
+++ modules/fleetcarrier.py
@@ -34,7 +34,6 @@ class FleetCarrierModule(Module):
                 data={"name": system, "x": x, "y": y, "z": z}
             )
         elif event in {"CarrierJumpRequest", "CarrierJumpCancelled"} and not entry.is_beta:
-            name = entry.data["StationName"]
             data = dict(
                 cmdr=entry.cmdr,
                 is_beta=entry.is_beta,
@@ -45,6 +44,6 @@ class FleetCarrierModule(Module):
             )
             global_context.cec_api.submit(
                 method="PUT",
-                url=f"{self.base_url}/{name}/jump",
+                url=f"{self.base_url}/{entry.station}/jump",
                 data=data
             )
