9d00c0e40ec42b4c05a767e86ebd70b9dac08bfc
Slava Kazakov <slavakazakov10@gmail.com>
2021-05-09 16:03:35 +0300

    Обновлена обёртка config для поддержки новых функций config.py коннектора

--- modules/lib/conf.py
+++ modules/lib/conf.py
@@ -7,10 +7,19 @@ class Config:
     """
     prefix = "Triumvirate"
     def getint(self, key):
-        return base_config.getint(f"{self.prefix}.{key}")
+        return base_config.get_int(f"{self.prefix}.{key}")
 
     def get(self, key):
         return base_config.get(f"{self.prefix}.{key}")
+
+    def get_bool(self, key):
+        return base_config.get_bool(f"{self.prefix}.{key}")
+    
+    def get_list(self, key):
+        return base_config.get_list(f"{self.prefix}.{key}")
+
+    def get_str(self, key):
+        return base_config.get_str(f"{self.prefix}.{key}")
 
     def set(self, key, val):
         base_config.set(f"{self.prefix}.{key}", val)
