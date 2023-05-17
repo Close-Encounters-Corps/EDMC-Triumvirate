7ac50355447e5f4df7fff086e5e0fdfd3dca678e
Artem.Zhukov <artem.zhukov@cosmos2.ru>
2023-05-17 08:52:08 +0300

    фÐadd new url

--- settings.py
+++ settings.py
@@ -1,7 +1,7 @@
 canonn_patrols_url = "https://docs.google.com/spreadsheets/d/e/2PACX-1vSMFJL2u0TbLMAQQ5zYixzgjjsNtGunZ9-PPZFheB4xzrjwR0JPPMcdMwqLm8ioVMp3MP4-k-JsIVzO/pub?gid=282559555&single=true&output=csv"
 bgs_tasks_url = "https://docs.google.com/spreadsheets/d/e/2PACX-1vQQZFJ4O0nb3L1WJk5oMEPJrr1w5quBSnPRwSbz66XCYx0Lq6aAexm9s1t8N8iRxpdbUOtrhKqQMayY/pub?gid=0&single=true&output=csv"
 edsm_poi_url = "https://www.edsm.net/en/galactic-mapping/json"
-
+edsm_url = "https://www.edsm.net"
 
 ####################
 ### CEC SETTINGS ###
