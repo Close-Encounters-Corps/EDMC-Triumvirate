bacbe340a4b48fb6f83c13e359e920d7e037f052
rinkulu <rinkulukat@gmail.com>
2023-09-18 18:15:25 +0300

    sending data to new forms

--- modules/legacy.py
+++ modules/legacy.py
@@ -617,17 +617,17 @@ class BGS():
                     factions_inf[faction["Faction"]] *= -1
             
             url_params = {
-                    "entry.1506409811": cmdr,
-                    "entry.1546958101": completed_mission["type"],
-                    "entry.450251211": "COMPLETED",
-                    "entry.542793243": completed_mission["system"],
-                    "entry.1404963760": completed_mission["faction"],
-                    "entry.340829640": factions_inf[completed_mission["faction"]],
-                    "entry.2011651184": completed_mission["system2"],
-                    "entry.1354651798": completed_mission["faction2"],
-                    "entry.342000821": factions_inf.get(completed_mission["faction2"], ""),
+                    "entry.1839270329": cmdr,
+                    "entry.1889332006": completed_mission["type"],
+                    "entry.350771392": "COMPLETED",
+                    "entry.592164382": completed_mission["system"],
+                    "entry.1812690212": completed_mission["faction"],
+                    "entry.179254259": factions_inf[completed_mission["faction"]],
+                    "entry.739461351": completed_mission["system2"],
+                    "entry.887402348": completed_mission["faction2"],
+                    "entry.1755429366": factions_inf.get(completed_mission["faction2"], ""),
                 }
-            url = f'{URL_GOOGLE}/1FAIpQLScbq22s4C-zGkVPamKZCqismCMfGgQ9hIfJbHznbiKSiIS-iw/formResponse?usp=pp_url&{"&".join([f"{k}={v}" for k, v in url_params.items()])}'
+            url = f'{URL_GOOGLE}/1FAIpQLSdlMUq4bcb4Pb0bUTx9C6eaZL6MZ7Ncq3LgRCTGrJv5yNO2Lw/formResponse?usp=pp_url&{"&".join([f"{k}={v}" for k, v in url_params.items()])}'
             print("\tMISSION_COMPLETE: link: " + url)
             Reporter(url).start()
             print("\tMISSION_COMPLETE: successfully sent to google sheet")
@@ -654,17 +654,17 @@ class BGS():
                 print("\tMISSION_FAILED: WARNING: mission not found, exiting")
 
             url_params = {
-                    "entry.1506409811": cmdr,
-                    "entry.1546958101": failed_mission["type"],
-                    "entry.450251211": "FAILED",
-                    "entry.542793243": failed_mission["system"],
-                    "entry.1404963760": failed_mission["faction"],
-                    "entry.340829640": -2,
-                    "entry.2011651184": failed_mission["system2"],
-                    "entry.1354651798": failed_mission["faction2"],
-                    "entry.342000821": "-2" if failed_mission["system2"] != "" else "",
+                    "entry.1839270329": cmdr,
+                    "entry.1889332006": failed_mission["type"],
+                    "entry.350771392": "FAILED",
+                    "entry.592164382": failed_mission["system"],
+                    "entry.1812690212": failed_mission["faction"],
+                    "entry.179254259": -2,
+                    "entry.739461351": failed_mission["system2"],
+                    "entry.887402348": failed_mission["faction2"],
+                    "entry.1755429366": "-2" if failed_mission["system2"] != "" else "",
                 }
-            url = f'{URL_GOOGLE}/1FAIpQLScbq22s4C-zGkVPamKZCqismCMfGgQ9hIfJbHznbiKSiIS-iw/formResponse?usp=pp_url&{"&".join([f"{k}={v}" for k, v in url_params.items()])}'
+            url = f'{URL_GOOGLE}/1FAIpQLSdlMUq4bcb4Pb0bUTx9C6eaZL6MZ7Ncq3LgRCTGrJv5yNO2Lw/formResponse?usp=pp_url&{"&".join([f"{k}={v}" for k, v in url_params.items()])}'
             print("\tMISSION_FAILED: link: " + url)
             Reporter(url).start()
             print("\tMISSION_FAILED: successfully sent to google sheet")
@@ -700,14 +700,14 @@ class BGS():
                         for faction in entry["Factions"]:
                             print("\tREDEEM_VOUCHER: current faction: " + str(faction))
                             url_params = {
-                                "entry.2049800763": cmdr,
-                                "entry.1945972044": "bounty",
-                                "entry.1944054969": system,
-                                "entry.251710042": "",
-                                "entry.284340607": faction["Faction"],
-                                "entry.375559118": faction["Amount"],
+                                "entry.503143076": cmdr,
+                                "entry.1108939645": "bounty",
+                                "entry.127349896": system,
+                                "entry.442800983": "",
+                                "entry.48514656": faction["Faction"],
+                                "entry.351553038": faction["Amount"],
                             }
-                            url = f'{URL_GOOGLE}/1FAIpQLSektTrrVVIJW1gMat7biNwZC0TWb0vLXP5BlXStcv3ep53t1Q/formResponse?usp=pp_url&{"&".join([f"{k}={v}" for k, v in url_params.items()])}'
+                            url = f'{URL_GOOGLE}/1FAIpQLSenjHASj0A0ransbhwVD0WACeedXOruF1C4ffJa_t5X9KhswQ/formResponse?usp=pp_url&{"&".join([f"{k}={v}" for k, v in url_params.items()])}'
                             print("\tREDEEM_VOUCHER: link: " + url)
                             Reporter(url).start()
                             print("\tREDEEM_VOUCHER: successfully sent to google sheet")
@@ -716,14 +716,14 @@ class BGS():
             elif "SellExplorationData" in entry["event"]:
                 print(f"\tSELL_EXP_DATA: detected \"{entry['event']}\"")
                 url_params = {
-                    "entry.2049800763": cmdr,
-                    "entry.1945972044": "SellExpData",
-                    "entry.1944054969": system,
-                    "entry.251710042": station,
-                    "entry.284340607": self.mainfaction,
-                    "entry.375559118": entry["TotalEarnings"],
+                    "entry.503143076": cmdr,
+                    "entry.1108939645": "SellExpData",
+                    "entry.127349896": system,
+                    "entry.442800983": station,
+                    "entry.48514656": self.mainfaction,
+                    "entry.351553038": entry["TotalEarnings"],
                 }
-                url = f'{URL_GOOGLE}/1FAIpQLSektTrrVVIJW1gMat7biNwZC0TWb0vLXP5BlXStcv3ep53t1Q/formResponse?usp=pp_url&{"&".join([f"{k}={v}" for k, v in url_params.items()])}'
+                url = f'{URL_GOOGLE}/1FAIpQLSenjHASj0A0ransbhwVD0WACeedXOruF1C4ffJa_t5X9KhswQ/formResponse?usp=pp_url&{"&".join([f"{k}={v}" for k, v in url_params.items()])}'
                 print("\tSELL_EXP_DATA: link: " + url)
                 Reporter(url).start()
                 print("\tSELL_EXP_DATA: successfully sent to google sheet")
--- modules/release.py
+++ modules/release.py
@@ -224,8 +224,8 @@ class Release(Frame, Module):
             self.grid_remove()
             return
         elif latest_version < self.version:
-            self.hyperlink["text"] = f"Тестовая версия {self.version.raw_value}.bgs17"
-            print("\tPLUGIN VERSION: 1.9.2 + post-exp + bgs17")
+            self.hyperlink["text"] = f"Тестовая версия {self.version.raw_value}.bgs18"
+            print("\tPLUGIN VERSION: 1.9.2 + post-exp + bgs18")
             return
         if self.no_auto_val == 1:
             debug("Automatic update disabled.")
