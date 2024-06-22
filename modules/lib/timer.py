1298e55c8dd93c3c9cdcf1c4c19c94c7641604c2
rinkulu <rinkulukat@gmail.com>
2024-06-22 15:04:20 +0300

    added a proper description for modules.lib.timer

--- modules/lib/timer.py
+++ modules/lib/timer.py
@@ -3,28 +3,30 @@ from modules.lib.thread import Thread, ThreadExit
 
 class Timer(Thread):
     """
-    Через secs секунд вызывает объект target.
+    Таймер для отложенного выполнения нужных нам действий.
     Постоянно проверяет, не следует ли ему самоубиться, дабы не блокировать закрытие EDMC,
     чем выгодно отличается от tk.after.
-
-    Параметр on_close позволяет определить, следует ли таймеру вызвать target
-    в случае закрытия EDMC до истечения времени secs (по-умолчанию True).
     """
-    def __init__(self, secs: float, target: Callable[[], Any], on_close: bool = True, **kwargs):
+    def __init__(self, secs: float, target: Callable[[], Any], run_on_closing: bool = True, **kwargs):
+        """
+        *secs* - время в секундах до вызова *target*.
+
+        *target* - объект, вызываемый по истечении таймера. Любой callable object.
+
+        *run_on_closing* - следует ли нам вызвать *target*, если EDMC будет закрываться до истечения таймера.
+        """
         name = kwargs.get("name", f"Timer before {target.__qualname__}")
         super().__init__(name=name, **kwargs)
         self.timeout = secs
         self.target = target
-        self.on_close = on_close
+        self.run_on_closing = run_on_closing
     
 
     def do_run(self):
         try:
             self.sleep(self.timeout)
-        except ThreadExit:
-            # бросается sleep-ом, если EDMC закрывается
-            if self.on_close:
+        except ThreadExit:      # бросается sleep-ом, если EDMC закрывается
+            if self.run_on_closing:
                 self.target()
-        else:
-            # а тут мы нормально дождались окончания таймера
+        else:                   # а тут мы нормально дождались окончания таймера
             self.target()
