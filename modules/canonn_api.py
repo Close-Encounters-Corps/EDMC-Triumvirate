864caf315e403ff07c75d3a9acb49e7a5a13ad4e
rinkulu <rinkulukat@gmail.com>
2025-02-01 22:52:15 +0300

    make CanonnAPI use its own Reporter

--- modules/canonn_api.py
+++ modules/canonn_api.py
@@ -3,12 +3,35 @@ from datetime import datetime, timedelta
 
 from context import PluginContext, GameState
 from settings import canonn_realtime_url
-from modules.legacy import Reporter
 from modules.debug import info, debug, error
 from modules.lib.module import Module
 from modules.lib.journal import JournalEntry
 from modules.lib.thread import Thread
 from modules.lib.timer import Timer
+
+
+class CanonnReporter(Thread):
+    """Смесь Reporter и Canonn-овского Emitter"""
+    # https://github.com/canonn-science/EDMC-Canonn/blob/05a5d9c4f842ad4fdb52b1a04ffc01ac623eba2b/canonn/emitter.py
+
+    def __init__(self, url: str, payload: dict):
+        super().__init__()
+        self.url = url
+        self.payload = payload
+
+    def do_run(self):
+        try:
+            res = requests.post(
+                self.url,
+                data=json.dumps(self.payload, ensure_ascii=False).encode('utf-8'),
+                headers={"content-type": "application/json"}
+            )
+            res.raise_for_status()
+        except requests.RequestException as e:
+            PluginContext.logger.error(
+                "[CanonnReporter] Couldn't send data to Canonn Cloud. Url: {}, payload: {}.".format(self.url, self.payload),
+                exc_info=e
+            )
 
 
 class HDDetector:
@@ -100,7 +123,7 @@ class HDDetector:
             "odyssey": GameState.odyssey,
             "hostile": self.status == self.HOSTILE
         }
-        Reporter(url, params).start()
+        CanonnReporter(url, params).start()
 
         # сбрасываем состояние до следующего перехвата
         self.status = self.SAFE
@@ -129,7 +152,7 @@ class HDDetector:
                 "timestamp": timestamp,
                 "x": x, "y": y, "z": z
             }
-            Reporter(url, params).start()
+            CanonnReporter(url, params).start()
 
 
 
@@ -223,7 +246,7 @@ class CanonnRealtimeAPI(Module):
             "rawEvents": [entry],
             "cmdrName":  journalEntry.cmdr
         }
-        Reporter(url, params).start()
+        CanonnReporter(url, params).start()
 
     
     def _dump_batch(self):
@@ -237,7 +260,7 @@ class CanonnRealtimeAPI(Module):
             "rawEvents": [entry.data for entry in self.fss_signals_batch],
             "cmdrName":  self.fss_signals_batch[0].cmdr
         }
-        Reporter(url, params.copy()).start()
+        CanonnReporter(url, params.copy()).start()
         self.fss_signals_batch.clear()
     
     
