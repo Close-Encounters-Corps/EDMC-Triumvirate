4d3aec846fc5d5e57b2f61d87139240fa383efc8
rinkulu <rinkulukat@gmail.com>
2025-03-08 11:45:53 +0300

    fix notifier's dark theme
    this time for sure

--- modules/notifier.py
+++ modules/notifier.py
@@ -10,26 +10,31 @@ import myNotebook as nb         # type: ignore
 
 
 class _Message(tk.Frame):
-    _cross_image: ImageTk.PhotoImage | None = None
+    _cross_image: ImageTk.PhotoImage = None
+    _cross_image_white: ImageTk.PhotoImage = None
 
     def __init__(self, master, text: str, timeout: int):
-        super().__init__(master, relief="groove")
+        super().__init__(master)
         self.grid_columnconfigure(0, weight=1)
 
         self._label = tk.Label(self, text=text, wraplength=self._get_wraplength())
-        self._label.grid(row=0, column=0, pady=2)
+        self._label.grid(row=0, column=0, pady=2, sticky="W")
 
-        if not self._cross_image:
-            self._set_image()
         self._button = nb.Button(self, image=self._cross_image)
-        self._button_dark = tk.Label(self, image=self._cross_image)
+        self._button_dark = tk.Label(self, image=self._cross_image_white)
+        grid_params = {"row": 0, "column": 1, "padx": 5, "pady": 2, "sticky": "E"}
         theme.register_alternate(
             (self._button, self._button_dark, self._button_dark),
-            {"row": 0, "column": 1, "padx": 5, "pady": 2}
+            grid_params
         )
         self._button.bind('<Button-1>', self._close)
         theme.button_bind(self._button_dark, self._close)
+        if theme.active == theme.THEME_DEFAULT:         # светлая тема
+            self._button.grid(**grid_params)
+        else:
+            self._button_dark.grid(**grid_params)
 
+        theme.update(self)
         self.pack(side="top", fill="x")
 
         if timeout != 0:
@@ -38,18 +43,21 @@ class _Message(tk.Frame):
         else:
             self._timer = None
 
-    def _close(self, event: tk.Event):
+    def _close(self, event: tk.Event | None = None):
         if self._timer:
             self._timer.kill()      # на случай, если уведомление скрыто раньше времени
         self.destroy()
         self.master._message_destroyed(self)
 
     @classmethod
-    def _set_image(cls):
-        size = int(16 * (tk._default_root.winfo_screenheight() / 1080))
+    def _set_images(cls):
+        size = int(14 * (tk._default_root.winfo_screenheight() / 1080))
         image = Image.open(Path(PluginContext.plugin_dir) / 'icons' / 'cross.png')
         image = image.resize((size, size))
         cls._cross_image = ImageTk.PhotoImage(image)
+        image_white = Image.open(Path(PluginContext.plugin_dir) / "icons" / "cross_white.png")
+        image_white = image_white.resize((size, size))
+        cls._cross_image_white = ImageTk.PhotoImage(image_white)
 
     @staticmethod
     def _get_wraplength():
@@ -69,6 +77,7 @@ class Notifier(tk.Frame):
 
     def __init__(self, parent, row):
         super().__init__(parent)
+        _Message._set_images()
         self.gridrow = row
         self._pool: list[_Message] = list()
 
