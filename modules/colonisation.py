67b87f785ed8f1a575809cb7c229d2ee7de6b2cc
rinkulu <rinkulukat@gmail.com>
2025-03-19 19:54:06 +0300

    add support for orbital constructions

--- modules/colonisation.py
+++ modules/colonisation.py
@@ -14,20 +14,28 @@ class DeliveryTracker(Module):
 
     def __init__(self):
         self.docked_on_cs: bool = False
+        self.construction_site: str | None = None
         self.cargo: dict[str, int] = dict()
 
     def on_journal_entry(self, entry: JournalEntry):
-        def on_colonisation_ship():
+        def _get_construction_type() -> str | None:
             station_name: str = entry.data.get("StationName", "")
-            return station_name == "System Colonisation Ship" or "$EXT_PANEL_ColonisationShip" in station_name
+            if station_name == "System Colonisation Ship" or "$EXT_PANEL_ColonisationShip" in station_name:
+                debug(f"Detected '{event}' on a colonisation ship (primary port construction site).")
+                return "Primary port"
+            # TODO: проверить на других локализациях
+            station_type: str = entry.data.get("StationType", "")
+            if station_type == "SpaceConstructionDepot":
+                future_name = station_name.split(': ')[1]
+                debug(f"Detected '{event}' on an orbital construction site; construction name - {future_name}.")
+                return f"Space construction: {future_name}"
+            # TODO: наземные постройки
+            return None
 
         event: str = entry.data["event"]
-        if event == "Location":
-            docked = entry.data.get("Docked", False)
-            colonisation_ship = on_colonisation_ship()
-            self.docked_on_cs = docked and colonisation_ship
-        elif event == "Docked":
-            self.docked_on_cs = on_colonisation_ship()
+        if event in ("Location", "Docked"):
+            self.construction_type = _get_construction_type()
+            self.docked_on_cs = self.construction_type is not None
         # брабфанка: игра может затупить с обновлением груза и прописать его после отстыковки,
         # поэтому вместо неё (event == "Undocked") будем отслеживать выход из инстанса
         elif (
@@ -35,6 +43,7 @@ class DeliveryTracker(Module):
             or event == "Music" and entry.data.get("MusicTrack") == "MainMenu"
         ):
             self.docked_on_cs = False
+            self.construction_site = None
         self.update_cargo(entry.state)
 
     def update_cargo(self, state: dict):
@@ -50,12 +59,18 @@ class DeliveryTracker(Module):
         self.cargo = new_cargo
 
     def _report(self, delivered: dict[str, int]):
-        debug(f"[ColonisationTracker] Detected colonisation delivery, cargo diff: {delivered}")
+        debug(
+            "Detected colonisation delivery: "
+            f"system: {GameState.system}, "
+            f"construction: {self.construction_type}, "
+            f"cargo diff: {delivered}"
+        )
         url = "https://docs.google.com/forms/d/e/1FAIpQLSdbG8pQUHDryAkd1ReEIEo25zOs6LUPErUElytgwI8wmfWAUA/formResponse?usp=pp_url"
         for item, amount in delivered.items():
             params = {
                 "entry.1492553995": GameState.cmdr,
                 "entry.639938351": GameState.system,
+                "entry.1059020094": self.construction_type,
                 "entry.173800538": item,
                 "entry.883168154": amount
             }
